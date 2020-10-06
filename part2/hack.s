7d47a9f37f2d0853f9cae502dc987faafa14ff21
Brendan McDonnell <brendanm149@gmail.com>
2020-10-06 13:51:02 -0600

    Parsed and commented conditionals

--- part2/hack.s
+++ part2/hack.s
@@ -23,8 +23,7 @@ main:
     la	$a0, outputMessage
     syscall
 
-    # print result
-    # thus, final result is stored in $s1
+    # print result (stored in $s1)
     li	$v0, 1
     move	$a0, $s1
     syscall
@@ -33,16 +32,25 @@ main:
     addi	$v0, $zero,	10
     syscall
 
+# option 2
+# multiplies user input by 2320
 cond2:
-    # option 2
     addi,	$t3, $zero, 2320
+    
+    # prompt for value
     li	$v0,	4
     la	$a0, valuePrompt
     syscall
+    
+    # get user input
     li	$v0, 5
     syscall
+    
+    # multiply input by 2320 and store in $s1
     move	$s1, $v0
     mul	$s1, $s1, $t3
+    
+    # return
     jr	$ra
 
 conditionalMultiplier:
@@ -50,26 +58,44 @@ conditionalMultiplier:
     beq	$v0, 2, cond2
     beq	$v0, 3, cond3 # if input is not 1, 2, or 3, default to input of 3
 
+# option 3
+# multiplies user input by 73
 cond3:
-    # option 3
     addi,	$t7, $zero, 73
+    
+    # prompt for value
     li	$v0, 4
     la	$a0, valuePrompt
     syscall
+    
+    # get user input
     li	$v0, 5
     syscall
+    
+    # multiply input by 73 and store in $s1
     move	$s1, $v0
     mul	$s1, $s1, $t7
+    
+    # return
     jr	$ra
 
+# option 1
+# multiples user input by 20
 cond1:
-    # option 1
     addi,	$t5, $zero, 20
+    
+    # prompt for value
     li	$v0, 4
     la	$a0, valuePrompt
     syscall
+    
+    # get user input
     li	$v0, 5
     syscall
+    
+    # multiply input by 73 and store in $s1
     move	$s1, $v0
     mul	$s1, $s1, $t5
+    
+    # return
     jr	$ra
