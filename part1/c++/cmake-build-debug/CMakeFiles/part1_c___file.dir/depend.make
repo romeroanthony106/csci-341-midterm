9713c8b2e7f697848154942842c5a5e0ad87a76c
romeroanthony106 <32376907+romeroanthony106@users.noreply.github.com>
2020-10-06 14:11:22 -0600

    finished c++ code still need to comment

---
+++ part1/c++/cmake-build-debug/CMakeFiles/part1_c___file.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for part1_c___file.
+# This may be replaced when dependencies are built.
--- part2/main.cpp
+++ part2/main.cpp
@@ -4,6 +4,50 @@
  * Through observation, hack.s multiplies an input but some constant that is selected based on user input
  */
 
+#include <iostream>
+using namespace std;
+
+int option1();
+int option2();
+int option3();
+
 int main() {
+    cout << "Enter 1, 2, or 3: " << endl;
+    int value;
+    cin >> value;
+
+    if(value == 1){
+        value = option1();
+    }
+    else if(value == 2){
+        value = option2();
+    }
+    else if(value == 3){
+        value = option3();
+    }
+    cout << "The new value is: " << value << endl;
+
+
     return 0;
+}
+
+int option1(){
+    int value = 0;
+    cout << "Enter a value" << endl;
+    cin >> value;
+    value = value * 20;
+}
+
+int option2(){
+    int value = 0;
+    cout << "Enter a value" << endl;
+    cin >> value;
+    value = value * 2320;
+}
+
+int option3(){
+    int value = 0;
+    cout << "Enter a value" << endl;
+    cin >> value;
+    value = value * 73;
 }
